4266c78f895f04c1109f7e6c3ed175af44a9547a
John James <jjames@pacbell.net>
2020-09-23 10:19:11 -0700

    Loaded All Swift Packages using Manager Copied files from Master Need to resolve replacement of app delegate and Scene Delegate into App

---
+++ MakeItSo/App/MakeItSoApp.swift
@@ -0,0 +1,17 @@
+//
+//  MakeItSoApp.swift
+//  MakeItSo
+//
+//  Created by John James Retina on 9/23/20.
+//
+
+import SwiftUI
+
+@main
+struct MakeItSoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
